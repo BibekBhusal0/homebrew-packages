522ec4a754f14c4115f64d44573c0c432c337a57
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-16 20:10:47 +0545

    update sha250

--- Formula/focusd.rb
+++ Formula/focusd.rb
@@ -2,7 +2,7 @@ class Focusd < Formula
   desc "Beautiful terminal pomodoro timer with daemon, waybar integration and TUI"
   homepage "https://github.com/bibekbhusal0/focusd"
   url "https://github.com/bibekbhusal0/focusd/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7728c4418914fb464b41733e45ca690bf0aafa030e91334b9124086d0bd3795a"
+  sha256 "206d8f7a46f335464d1e05555cae1317330d393270c40a30d765896dd5d4a1a2"
   license "MIT"
   head "https://github.com/bibekbhusal0/focusd.git", branch: "master"
 
