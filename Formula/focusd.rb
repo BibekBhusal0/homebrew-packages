f59cb8a815d29b01954c657f1cb72a1cd2e556e1
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-16 17:53:18 +0545

    fix: 🐛 tag

--- Formula/focusd.rb
+++ Formula/focusd.rb
@@ -1,7 +1,7 @@
 class Focusd < Formula
   desc "Beautiful terminal pomodoro timer with daemon, waybar integration and TUI"
   homepage "https://github.com/bibekbhusal0/focusd"
-  url "https://github.com/bibekbhusal0/focusd/archive/refs/tags/v-0.1.0.tar.gz"
+  url "https://github.com/bibekbhusal0/focusd/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "7728c4418914fb464b41733e45ca690bf0aafa030e91334b9124086d0bd3795a"
   license "MIT"
   head "https://github.com/bibekbhusal0/focusd.git", branch: "master"
