047fa5d7d484438050fd987839141f5617835406
BibekBhusal0 <bibekbhusal100@gmail.com>
2026-09-01 23:16:53 +0545

    chore: update focusd to v0.2.13

--- Formula/focusd.rb
+++ Formula/focusd.rb
@@ -1,8 +1,8 @@
 class Focusd < Formula
   desc "Beautiful terminal pomodoro timer with daemon, waybar integration and TUI"
   homepage "https://github.com/bibekbhusal0/focusd"
-  url "https://github.com/bibekbhusal0/focusd/archive/refs/tags/v0.2.12.tar.gz"
-  sha256 "0f8ffdc77afcf74c0e41f02213563f1d55dc9bb67135dd071d10479e24e9d5b4"
+  url "https://github.com/bibekbhusal0/focusd/archive/refs/tags/v0.2.13.tar.gz"
+  sha256 "f732f3e87d3987e7f5fad2b803df3985a144db973f3b8ce02b0859a2681137ec"
   license "MIT"
   head "https://github.com/bibekbhusal0/focusd.git", branch: "master"
 
